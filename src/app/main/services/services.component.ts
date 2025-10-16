9271551259cbd66e24795482dda656db60d0b387
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2025-10-16 11:28:08 -0500

    add bootcamp github actions

--- src/app/main/services/services.component.ts
+++ src/app/main/services/services.component.ts
@@ -10,33 +10,39 @@ export class ServicesComponent {
 
   certifications = [
     {
+      title: 'GitHub Actions', date: 'Feb. 2025',
+      description: 'Bootcamp de GitHub Actions cursada en la Plataforma Código Facilito',
+      pathFile: 'assets/files/certifications/github_actions.pdf',
+      nameFile: 'gitHub_actions.pdf'
+    },
+    {
       title: 'Arquitectura de Software', date: 'Feb. 2025',
       description: 'Bootcamp de Arquitectura de Software cursada en la Plataforma Código Facilito',
-      pahtFile: 'assets/files/certifications/arquitectura _de_software.pdf',
+      pathFile: 'assets/files/certifications/arquitectura _de_software.pdf',
       nameFile: 'arquitectura _de_software.pdf'
     },
     {
       title: 'Gestión de Proyectos', date: 'Nov. 2024',
       description: 'Especialización en Gestión de Proyectos por la Universidad Tecnológica Nacional del Perú',
-      pahtFile: 'assets/files/certificationes/gestion_proyectos_1.pdf',
+      pathFile: 'assets/files/certifications/gestion_proyectos_1.pdf',
       nameFile: 'gestion_proyectos_1.pdf'
     },
     {
       title: 'Certificación Scrum Master', date: 'May. 2024',
       description: 'Certificación Scrum Master por la institución certificadora SCRUMstudy',
-      pahtFile: 'assets/files/certificationes/scrum_master_scrum_study.pdf',
+      pathFile: 'assets/files/certifications/scrum_master_scrum_study.pdf',
       nameFile: 'scrum_master_scrum_study.pdf'
     },
     {
       title: 'Capacitación Scrum Master', date: 'May. 2024',
       description: 'Capacitación en Scrum Master cursado en la Pontifice Universidad Católica del Perú',
-      pahtFile: 'assets/files/certificationes/cap_scrum_master.pdf',
+      pathFile: 'assets/files/certifications/cap_scrum_master.pdf',
       nameFile: 'cap_scrum_master.pdf'
     },
     {
       title: 'Inglés Americano Intermedio', date: 'Nov. 2021',
       description: 'Inglés americano intermedio cursado en la Universidad Nacional Agraria de la Selva',
-      pahtFile: 'assets/files/certificationes/ingles_intermedio_unas.pdf',
+      pathFile: 'assets/files/certifications/ingles_intermedio_unas.pdf',
       nameFile: 'ingles_intermedio_unas.pdf'
     },
   ]
